fe12dce25771bfcb3588eca5785d27581c4ede6b
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2025-06-17 12:35:59 +0300

    Initial Commit

---
+++ 24HSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24HSoft
+//
+//  Created by Alexandr Onischenko on 17.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
